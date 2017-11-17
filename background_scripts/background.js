58d924a2bf37e78b0c9a8c67d9b64ff2bc46434a
Hendrik Pastunink <HandyP94@gmail.com>
2017-11-17 21:21:19 +0100

    Now using html injection to display an input on the current site

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -19,7 +19,9 @@ function init() {
 
 function addListeners() {
     browser.commands.onCommand.addListener((command) => {
-        if (command.startsWith("ctrl+")) {
+        if (command === "ctrl+q") {
+            onOpenSpeedDial();
+        } else if (command.startsWith("ctrl+")) {
             onCtrl(Number.parseInt(command.charAt(command.length - 1)));
         }
     });
@@ -28,6 +30,23 @@ function addListeners() {
         if (alarm.name === "timeout") {
             onTimeout();
         }
+    });
+
+    browser.runtime.onMessage.addListener((message) => {
+        let number = Number.parseInt(message.number, 10);
+        if (number !== NaN) {
+            openNumber = number;
+            numberLength = message.number.length;
+            tryOpen();
+        }
+    });
+}
+
+function onOpenSpeedDial() {
+    getActiveTab((tab) => {
+        browser.tabs.sendMessage(tab.id, "openSpeedDial");
+    }, () => {
+        console.log("couldn't get active tab");
     });
 }
 
@@ -71,7 +90,7 @@ function tryOpen() {
 function tryOpenNumber() {
     if (openNumber !== 0) {
         console.log("number: " + openNumber);
-        openTabForeground(getUrlForNumber(openNumber));
+        updateCurrentTab(getUrlForNumber(openNumber));
     } else {
         console.log("number was 0");
     }
@@ -80,11 +99,11 @@ function tryOpenNumber() {
 function getUrlForNumber(number) {
     if (settings !== undefined) {
         let url = settings.shortcuts[number];
-        if(url !== undefined && url.length > 0) {
+        if (url !== undefined && url.length > 0) {
             return url;
         }
     }
-    return "https://www.google.com";
+    return null;
 }
 
 function openTabForeground(url) {
@@ -99,6 +118,37 @@ function openTabForeground(url) {
     });
 }
 
+function getActiveTab(callback, error) {
+    let tabs = browser.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+    tabs.then((tabArray) => {
+        if (tabArray.length > 0) {
+            callback(tabArray[0]);
+        } else {
+            error();
+        }
+    }, error);
+}
+
+function updateCurrentTab(url) {
+    if (url !== null) {
+        getActiveTab((tab) => {
+            let update = browser.tabs.update(tab.id, {
+                url: url
+            });
+            update.then(() => {
+                console.log("opened in current tab: " + url);
+            }, () => {
+                console.log("couldn't update current tab:" + url)
+            });
+        }, () => {
+            console.log("couldn't query tabs")
+        });
+    }
+}
+
 function loadJson(url, callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
@@ -108,7 +158,7 @@ function loadJson(url, callback) {
 
             // .open will NOT return a value but simply returns undefined in async mode so use a callback
             callback(JSON.parse(xobj.responseText));
-        } 
+        }
     }
     xobj.send(null);
 }
---
+++ content_scripts/content_script.js
@@ -0,0 +1,103 @@
+const idRoot = "SpeedDialIdRoot";
+const DELAY = 1000;
+
+let timeoutId;
+
+init();
+
+function init() {
+    injectUI();
+    addListeners();
+}
+
+function addListeners() {
+    browser.runtime.onMessage.addListener((message) => {
+        if (message === "openSpeedDial") {
+            openSpeedDial();
+            restartTimeout();
+        };
+    });
+}
+
+function injectUI() {
+    if ($("#" + idRoot + "outerDiv").length === 0) {
+        $("body").append(generateUI());
+        hideUI();
+        addInputScripts();
+    }
+}
+
+function addInputScripts() {
+    let input = $("#" + idRoot + "input");
+    input.keypress((event) => {
+        var charCode = (event.which) ? event.which : event.keyCode
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
+        }
+        return true;
+    });
+    input.on("input", (e) => {
+        console.log("input");
+        if (input.val().length === 3) {
+            sendNumberToBackground(input.val());
+        } else {
+            restartTimeout();
+        }
+    });
+}
+
+function sendNumberToBackground(number) {
+    browser.runtime.sendMessage({
+        number: number
+    }).then(() => {
+        return
+    }, () => {
+        console.log("error")
+    });
+    hideUI();
+}
+
+function hideUI() {
+    $("#" + idRoot + "outerDiv").hide(0);
+    $("#" + idRoot + "input").val("");
+    $("#" + idRoot + "input").blur();
+    $("#" + idRoot + "input").prop("disabled", true);
+}
+
+function restartTimeout() {
+    console.log("restartTimeout")
+    if (timeoutId !== undefined) {
+        window.clearTimeout(timeoutId);
+    }
+    timeoutId = window.setTimeout(onTimeout, DELAY)
+}
+
+function onTimeout() {
+    let input = $("#" + idRoot + "input");
+    if (input.val().length > 0) {
+        sendNumberToBackground(input.val());
+    }
+    hideUI();
+}
+
+function generateUI() {
+    let div = $("<div>", {
+        "id": idRoot + "outerDiv"
+    })
+    let innerDiv = $("<div>", {
+        "id": idRoot + "innerDiv"
+    });
+    innerDiv.append($("<input>", {
+        "id": idRoot + "input",
+        "type": "text",
+        "maxlength": "3"
+    }));
+    div.append(innerDiv);
+    return div;
+}
+
+function openSpeedDial() {
+    $("#" + idRoot + "outerDiv").show(0);
+    $("#" + idRoot + "input").prop("disabled", false);
+    $("#" + idRoot + "input").focus();
+}
