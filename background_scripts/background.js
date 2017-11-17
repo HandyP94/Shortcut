dff437f2a7d0e6fb0c42c75b07ccac8cfcca3093
Hendrik Pastunink <HandyP94@gmail.com>
2017-11-17 23:27:07 +0100

    remove json loading

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -7,9 +7,6 @@ let openNumber = 0;
 init();
 
 function init() {
-    // loadJson("settings.json", (json) => {
-    //     settings = json;
-    // });
     loadFromStorage();
     addListeners();
 };
@@ -121,18 +118,4 @@ function updateCurrentTab(url) {
             console.log("couldn't query tabs")
         });
     }
-}
-
-function loadJson(url, callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', url, true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == 200) {
-
-            // .open will NOT return a value but simply returns undefined in async mode so use a callback
-            callback(JSON.parse(xobj.responseText));
-        }
-    }
-    xobj.send(null);
 }
